eb59db21ae8835959db833ae14c5d4be5e680c7d
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-09-07 16:07:30 -0300

    Funcion de editar producto añadida y correciones en la func de borrar producto

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,14 @@
-import {validarDescription, validarEmail, validarGenero, validarImagen, validarNombre, validarCantidad} from "./validaciones.js"
-import {Producto} from "./classProducto.js"
+import {
+  validarDescription,
+  validarEmail,
+  validarGenero,
+  validarImagen,
+  validarNombre,
+  validarCantidad,
+} from "./validaciones.js";
+import { Producto } from "./classProducto.js";
 
-
-let listaProductos = []
+let listaProductos = [];
 
 //traer los input
 let codigo = document.querySelector("#codigo");
@@ -13,69 +19,94 @@ let imagen = document.querySelector("#imagen");
 let genero = document.querySelector("#genero");
 
 let btnCrearProducto = document.querySelector("#btnCrearProducto");
-let formProducto = document.querySelector("#formProducto")
+let formProducto = document.querySelector("#formProducto");
 // crear una instancia de la ventana modal
 const modalAdminProducto = new bootstrap.Modal(
   document.querySelector("#modalProducto")
 );
 
-nombre.addEventListener('blur',()=>{validarNombre(nombre)});
-descripcion.addEventListener('blur',()=>{validarDescription(descripcion)});
-imagen.addEventListener('blur',()=>{validarImagen(imagen)})
-cantidad.addEventListener('blur',()=>{validarCantidad(cantidad)})
-genero.addEventListener('blur',()=>{validarGenero(genero)})
+let productoNuevo = true;
 
+nombre.addEventListener("blur", () => {
+  validarNombre(nombre);
+});
+descripcion.addEventListener("blur", () => {
+  validarDescription(descripcion);
+});
+imagen.addEventListener("blur", () => {
+  validarImagen(imagen);
+});
+cantidad.addEventListener("blur", () => {
+  validarCantidad(cantidad);
+});
+genero.addEventListener("blur", () => {
+  validarGenero(genero);
+});
 
-
-btnCrearProducto.addEventListener('click', crearProducto);
-formProducto.addEventListener('submit', guardarProducto);
+btnCrearProducto.addEventListener("click", crearProducto);
+formProducto.addEventListener("submit", guardarProducto);
 //variable para ejecutar la funcion que crea peliculas o la que actualiza
-function crearProducto(){
-    modalAdminProducto.show();
-    codigo.value = uuidv4();
+function crearProducto() {
+  productoNuevo = true;
+  limpiarFormulario();
+  modalAdminProducto.show();
+  codigo.value = uuidv4();
 }
 
-function guardarProducto(e){
-    e.preventDefault();
-   //volver a validar todos los campos
+function guardarProducto(e) {
+  e.preventDefault();
+  //volver a validar todos los campos
 
-   if(validarNombre(nombre) && validarDescription(descripcion) && validarImagen(imagen) && validarCantidad(cantidad) && validarGenero(genero)){
-   //si los datos son correctos
-   let nuevoProducto =  new Producto(codigo.value, nombre.value, descripcion.value, imagen.value, cantidad.value, genero.value);
-   
-   console.log(nuevoProducto)
-   listaProductos.push(nuevoProducto);
-   guardarProductosEnLocalStorage();
-crearLista(nuevoProducto)
-   //limpiar formulario
-   limpiarFormulario();
-   console.log(listaProductos);
-   //cerrar la ventana modal
-   modalAdminProducto.hide();
-}
-   
-   
+  if (
+    validarNombre(nombre) &&
+    validarDescription(descripcion) &&
+    validarImagen(imagen) &&
+    validarCantidad(cantidad) &&
+    validarGenero(genero) && productoNuevo
+  ) {
+    //si los datos son correctos
+    let nuevoProducto = new Producto(
+      codigo.value,
+      nombre.value,
+      descripcion.value,
+      imagen.value,
+      cantidad.value,
+      genero.value
+    );
+
+    console.log(nuevoProducto);
+    listaProductos.push(nuevoProducto);
+    guardarProductosEnLocalStorage();
+    crearLista(nuevoProducto);
+    //limpiar formulario
+    limpiarFormulario();
+    console.log(listaProductos);
+    //cerrar la ventana modal
+    modalAdminProducto.hide();
+  } else {
+    actualizarProducto();
+  }
 }
 
-function limpiarFormulario(){
-    formProducto.reset()
-    // modificar las clases de bootstrap si es necesario
+function limpiarFormulario() {
+  formProducto.reset();
+  // modificar las clases de bootstrap si es necesario
 }
 
 const cargaInicial = () => {
-    listaProductos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
-    if(listaProductos.length > 0){
-        listaProductos.forEach(listaProducto => {
-            crearLista(listaProducto)
-        })
-    }
-}
+  listaProductos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+  if (listaProductos.length > 0) {
+    listaProductos.forEach((listaProducto) => {
+      crearLista(listaProducto);
+    });
+  }
+};
 
-function crearLista(producto){
-    //esta funcion dibuja un tr
-    let tablaProductos = document.querySelector('#tablaProductos');
-    //creamos el tr con document.createElement o innerHTML del tbody
-    tablaProductos.innerHTML += `<tr>
+function crearLista(producto) {
+  //esta funcion dibuja un tr
+  let tablaProductos = document.querySelector("#tablaProductos");
+  //creamos el tr con document.createElement o innerHTML del tbody
+  tablaProductos.innerHTML += `<tr>
     <th scope="row">${producto.codigo}</th>
     <td>${producto.nombre}</td>
     <td>${producto.descripcion}</td>
@@ -83,24 +114,24 @@ function crearLista(producto){
     <td>${producto.cantidad}</td>
     <td>${producto.genero}</td>
     <td>
-      <button class="btn btn-warning" >
+      <button class="btn btn-warning" onclick='modificarProducto("${producto.codigo}")'>
         <i class="bi bi-pencil-square"></i>
       </button>
       <button class="btn btn-danger" onclick='borrarProducto("${producto.codigo}")'>
         <i class="bi bi-x-square"></i>
       </button>
     </td>
-  </tr>`
-    console.log(tablaProductos)
+  </tr>`;
+  console.log(tablaProductos);
 }
 
-function guardarProductosEnLocalStorage(){
-    localStorage.setItem('listaProductosKey', JSON.stringify(listaProductos));
+function guardarProductosEnLocalStorage() {
+  localStorage.setItem("listaProductosKey", JSON.stringify(listaProductos));
 }
 cargaInicial();
 
 window.borrarProducto = function (codigo) {
-  //mostrar una pregunta al usuario
+  //Alert para confirmar la eliminacion del producto
   Swal.fire({
     title: "Borrar producto",
     text: "¿Esta seguro de eliminar el producto? Este proceso no se puede revertir",
@@ -113,20 +144,18 @@ window.borrarProducto = function (codigo) {
   }).then((result) => {
     console.log(result);
     if (result.isConfirmed) {
-      //aqui quiero borrar la producto
-      // console.log(codigo)
-      //buscar la producto en el arreglo y borrarla
+      //Buscar el producto en el arreglo y borrarlo
       let copiaListaProductos = listaProductos.filter(
         (itemProducto) => itemProducto.codigo != codigo
       );
       listaProductos = copiaListaProductos;
-      //actualizar el localstorage
+      //Actualizar el localstorage
       guardarProductosEnLocalStorage();
-      //actualizar la tabla
+      //Actualizar la tabla
       borrarTabla();
       cargaInicial();
 
-      // mostrar mensaje de operacion correcta
+      // Alert de borrado exitoso
       Swal.fire(
         "Producto borrado",
         "El producto se elimino correctamente",
@@ -135,9 +164,54 @@ window.borrarProducto = function (codigo) {
     }
   });
 };
-
 function borrarTabla() {
   let tablaProductos = document.querySelector("#tablaProductos");
   tablaProductos.innerHTML = "";
+}
+
+window.modificarProducto = function (codigoBuscado) {
+  //Cambio de estado de la variable productoNuevo
+  productoNuevo = false;
+  //buscar en el arreglo el producto que quiero editar
+  let productoBuscado = listaProductos.find(
+    (producto) => producto.codigo === codigoBuscado
+  );
+
+  //Abrir ventana modal con el formulario cargado con todos los datos del producto que se selecciono
+  modalAdminProducto.show();
+  codigo.value = productoBuscado.codigo;
+  nombre.value = productoBuscado.nombre;
+  descripcion.value = productoBuscado.descripcion;
+  cantidad.value = productoBuscado.cantidad;
+  imagen.value = productoBuscado.imagen;
+  genero.value = productoBuscado.genero;
 };
 
+function actualizarProducto() {
+  //Tomar los datos cargados en el formulario, buscar el objeto que se esta editando y actualizar sus valores
+  let posicionProductoBuscado = listaProductos.findIndex(
+    (producto) => codigo.value === producto.codigo
+  );
+
+  //Modificar los valores dentro del arreglo
+  listaProductos[posicionProductoBuscado].nombre = nombre.value;
+  listaProductos[posicionProductoBuscado].descripcion = descripcion.value;
+  listaProductos[posicionProductoBuscado].cantidad = cantidad.value;
+  listaProductos[posicionProductoBuscado].imagen = imagen.value;
+  listaProductos[posicionProductoBuscado].genero = genero.value;
+  //Actualizar el localstorage
+  guardarProductosEnLocalStorage();
+  //Actualizar la tabla
+  borrarTabla();
+  cargaInicial();
+  //Alert de confirmacion de cambios
+  Swal.fire(
+    "El producto se actualizo correctamente",
+    "Los datos delproducto seleccionado fueron actualizados",
+    "success"
+  );
+  //cerrar la ventana modal
+  modalAdminProducto.hide();
+  //limpiar el formulario
+  limpiarFormulario();
+}
