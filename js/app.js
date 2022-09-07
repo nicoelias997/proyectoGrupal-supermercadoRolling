e3c9c9e1da726b05e11acf79026ea46de1de493d
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-09-07 20:07:25 -0300

    Funcion para mostrar los producos del localStorage en index agregada

--- js/app.js
+++ js/app.js
@@ -15,3 +15,45 @@
 //             footer: '<a href="">Why do I have this issue?</a>'
 //           })
 //     }
+
+//verificar si el localstorage tiene datos
+let listaProductos =
+  JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+let grillaProductos = document.querySelector("#grillaProductos");
+
+//chequear si el arreglo listaProductos tiene algo
+if (listaProductos.length > 0) {
+  //entonces dibujo las cards
+  listaProductos.map((producto) => {
+    crearColumna(producto);
+  });
+} else {
+  //mostrar un mensaje intuitivo para el usuario
+}
+
+function crearColumna(producto) {
+  //traer el elemento grillaProductos de las columnas
+  // dibujar una columna con su respectiva card
+  grillaProductos.innerHTML += `
+  <article class="card m-2 p-1" style="width: 15rem;">
+  <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+  <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${producto.nombre}</li>
+      <li class="list-group-item">${producto.cantidad}</li>
+      <li class="list-group-item">$ ${producto.precio}</li>
+    </ul>
+    <button class="col-12 btn btn-danger agregarCarro" type="submit">
+      Agregar
+    </button>
+  </div>
+</article>`;
+}
+
+// function verDetalle(codigo) {
+//   console.log(codigo);
+//   console.log(window.location);
+//   console.log(window.location.origin + "/pages/detalle.html?codigo=" + codigo);
+//   window.location.href =
+//     window.location.origin + "/pages/detalle.html?codigo=" + codigo;
+// }
