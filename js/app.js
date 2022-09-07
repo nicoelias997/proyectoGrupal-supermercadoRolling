6930e6449b571d143aa5f7046b4ac4d7d4afadb0
nicoelias997 <nicoelias997@gmail.com>
2022-09-07 16:10:52 +0200

    Agrego un poco de maquetado a las card de productos, intento darle el mejor maquetado para luego poder realziar las funcionalidades, luego le cambiamos un poco mas de estilo y colores

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// import { validarEmail } from "./validaciones";
+
+// const email = document.getElementById("email").value
+// const btnIdentificarse = document.getElementById("btn-identificarse");
+// btnIdentificarse.addEventListener("click", (() => {identificarse}))
+
+// const identificarse = () => {
+//     if(validarEmail(email)){
+//         Swal.fire('Any fool can use a computer')
+//     } else {
+//         Swal.fire({
+//             icon: 'error',
+//             title: 'Oops...',
+//             text: 'Something went wrong!',
+//             footer: '<a href="">Why do I have this issue?</a>'
+//           })
+//     }
+// } 
