6230f0ec7077ffb08e4f164460968b21042ae410
nicoelias997 <nicoelias997@gmail.com>
2022-09-07 19:17:48 +0200

    Agrego validaciones para la ventana modal, no me funciona bien la de validar tamñao del producto, pendiente de correccion

--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,6 @@
-class Producto {
-    constructor(codigo, nombre, descripcion, imagen, tamanio, genero){
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.imagen = imagen;
-        this.tamanio = tamanio;
-        this.genero = genero;
-    }
-}
+import {validarDescription, validarEmail, validarGenero, validarImagen, validarNombre, validarTamanio} from "./validaciones.js"
+import {Producto} from "./classProducto.js"
+
 
 let listaProductos = []
 
@@ -26,6 +19,14 @@ const modalAdminProducto = new bootstrap.Modal(
   document.querySelector("#modalProducto")
 );
 
+nombre.addEventListener('blur',()=>{validarNombre(nombre)});
+descripcion.addEventListener('blur',()=>{validarDescription(descripcion)});
+imagen.addEventListener('blur',()=>{validarImagen(imagen)})
+tamanio.addEventListener('blur',()=>{validarTamanio(tamanio)})
+genero.addEventListener('blur',()=>{validarGenero(genero)})
+
+
+
 btnCrearProducto.addEventListener('click', crearProducto);
 formProducto.addEventListener('submit', guardarProducto);
 //variable para ejecutar la funcion que crea peliculas o la que actualiza
@@ -38,8 +39,10 @@ function guardarProducto(e){
     e.preventDefault();
    //volver a validar todos los campos
 
+   if(validarNombre(nombre) && validarDescription(descripcion) && validarImagen(imagen) && validarTamanio(tamanio) && validarGenero(genero)){
    //si los datos son correctos
    let nuevoProducto =  new Producto(codigo.value, nombre.value, descripcion.value, imagen.value,tamanio.value, genero.value);
+   
    console.log(nuevoProducto)
    listaProductos.push(nuevoProducto);
    //limpiar formulario
@@ -47,6 +50,8 @@ function guardarProducto(e){
    console.log(listaProductos);
    //cerrar la ventana modal
    modalAdminProducto.hide();
+}
+   
 }
 
 function limpiarFormulario(){
