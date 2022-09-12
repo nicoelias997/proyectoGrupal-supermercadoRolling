7e5a31c941611d9c12c875612e6d1edbc8a6d487
manuel ibanez <manuelibanezastorga@gmail.com>
2022-09-12 17:41:54 -0300

    agrego el buscador falta funcionalidad

---
+++ js/buscador.js
@@ -0,0 +1,13 @@
+document.addEventListener("keyup", e=>{
+ 
+   if(e.target.matches('#buscador')){
+   
+    if(e.key === 'Escape')e.target.value = ''
+
+       document.querySelectorAll('.articulo').forEach(fruta =>{
+
+           fruta.textContent.toLowerCase().includes(e.target.value.toLoLowerCase())?fruta.classList.remove('filtro')
+           :fruta.classList.add('filtro')
+       })
+   }
+})
