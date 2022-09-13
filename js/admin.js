473988d31812e3629e85e7f4ce55802f8c04a6be
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-09-13 14:56:49 -0300

    Correcciones en la pagina de administrador

--- js/admin.js
+++ js/admin.js
@@ -1,101 +1,227 @@
-import {validarDescription, validarEmail, validarGenero, validarImagen, validarNombre, validarCantidad} from "./validaciones.js"
-import {Producto} from "./classProducto.js"
+import {
+  validarDescription,
+  validarEmail,
+  validarGenero,
+  validarImagen,
+  validarNombre,
+  validarCantidad,
+  validarPrecio,
+} from "./validaciones.js";
+import { Producto } from "./classProducto.js";
 
-
-let listaProductos = []
+let listaProductos = [];
 
 //traer los input
 let codigo = document.querySelector("#codigo");
 let nombre = document.querySelector("#nombreProd");
 let descripcion = document.querySelector("#descripcion");
-let cantidad = document.querySelector("#cantidad");
 let imagen = document.querySelector("#imagen");
+let cantidad = document.querySelector("#cantidad");
+let precio = document.querySelector("#precio");
 let genero = document.querySelector("#genero");
 
 let btnCrearProducto = document.querySelector("#btnCrearProducto");
-let formProducto = document.querySelector("#formProducto")
+let formProducto = document.querySelector("#formProducto");
 // crear una instancia de la ventana modal
 const modalAdminProducto = new bootstrap.Modal(
   document.querySelector("#modalProducto")
 );
 
-nombre.addEventListener('blur',()=>{validarNombre(nombre)});
-descripcion.addEventListener('blur',()=>{validarDescription(descripcion)});
-imagen.addEventListener('blur',()=>{validarImagen(imagen)})
-cantidad.addEventListener('blur',()=>{validarCantidad(cantidad)})
-genero.addEventListener('blur',()=>{validarGenero(genero)})
+let productoNuevo = true;
 
+nombre.addEventListener("blur", () => {
+  validarNombre(nombre);
+});
+descripcion.addEventListener("blur", () => {
+  validarDescription(descripcion);
+});
+imagen.addEventListener("blur", () => {
+  validarImagen(imagen);
+});
+cantidad.addEventListener("blur", () => {
+  validarCantidad(cantidad);
+});
+precio.addEventListener("blur", () => {
+  validarPrecio(precio);
+});
+genero.addEventListener("blur", () => {
+  validarGenero(genero);
+});
 
-
-btnCrearProducto.addEventListener('click', crearProducto);
-formProducto.addEventListener('submit', guardarProducto);
+btnCrearProducto.addEventListener("click", crearProducto);
+formProducto.addEventListener("submit", guardarProducto);
 //variable para ejecutar la funcion que crea peliculas o la que actualiza
-function crearProducto(){
-    modalAdminProducto.show();
-    codigo.value = uuidv4();
+function crearProducto() {
+  productoNuevo = true;
+  limpiarFormulario();
+  modalAdminProducto.show();
+  codigo.value = uuidv4();
 }
 
-function guardarProducto(e){
-    e.preventDefault();
-   //volver a validar todos los campos
+function guardarProducto(e) {
+  e.preventDefault();
+  //volver a validar todos los campos
 
-   if(validarNombre(nombre) && validarDescription(descripcion) && validarImagen(imagen) && validarCantidad(cantidad) && validarGenero(genero)){
-   //si los datos son correctos
-   let nuevoProducto =  new Producto(codigo.value, nombre.value, descripcion.value, imagen.value,cantidad.value, genero.value);
-   
-   console.log(nuevoProducto)
-   listaProductos.push(nuevoProducto);
-   guardarProductosEnLocalStorage();
-crearLista(nuevoProducto)
-   //limpiar formulario
-   limpiarFormulario();
-   console.log(listaProductos);
-   //cerrar la ventana modal
-   modalAdminProducto.hide();
-}
-   
-   
+  if (
+    validarNombre(nombre) &&
+    validarDescription(descripcion) &&
+    validarImagen(imagen) &&
+    validarCantidad(cantidad) &&
+    validarPrecio(precio) &&
+    validarGenero(genero) && productoNuevo
+  ) {
+    //si los datos son correctos
+    let nuevoProducto = new Producto(
+      codigo.value,
+      nombre.value,
+      descripcion.value,
+      imagen.value,
+      cantidad.value,
+      precio.value,
+      genero.value
+    );
+
+    console.log(nuevoProducto);
+    listaProductos.push(nuevoProducto);
+    guardarProductosEnLocalStorage();
+    crearLista(nuevoProducto);
+    //limpiar formulario
+    limpiarFormulario();
+    console.log(listaProductos);
+    //cerrar la ventana modal
+    modalAdminProducto.hide();
+  } else {
+    actualizarProducto();
+  }
 }
 
-function limpiarFormulario(){
-    formProducto.reset()
-    // modificar las clases de bootstrap si es necesario
+function limpiarFormulario() {
+  formProducto.reset();
+  // modificar las clases de bootstrap si es necesario
 }
 
 const cargaInicial = () => {
-    listaProductos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
-    if(listaProductos.length > 0){
-        listaProductos.forEach(listaProducto => {
-            crearLista(listaProducto)
-        })
-    }
-}
+  listaProductos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+  if (listaProductos.length > 0) {
+    listaProductos.forEach((listaProducto) => {
+      crearLista(listaProducto);
+    });
+  }
+};
 
-function crearLista(producto){
-    //esta funcion dibuja un tr
-    let tablaProductos = document.querySelector('#tablaProductos');
-    //creamos el tr con document.createElement o innerHTML del tbody
-    tablaProductos.innerHTML += `<tr class="align-middle">
+function crearLista(producto) {
+  //esta funcion dibuja un tr
+  let tablaProductos = document.querySelector("#tablaProductos");
+  //creamos el tr con document.createElement o innerHTML del tbody
+  tablaProductos.innerHTML += `<tr>
     <th scope="row">${producto.codigo}</th>
     <td>${producto.nombre}</td>
     <td>${producto.descripcion}</td>
     <td>${producto.imagen}</td>
     <td>${producto.cantidad}</td>
+    <td>${producto.precio}</td>
     <td>${producto.genero}</td>
     <td>
-      <button class="btn btn-warning" >
+      <button class="btn btn-warning" onclick='modificarProducto("${producto.codigo}")'>
         <i class="bi bi-pencil-square"></i>
       </button>
-      <button class="btn btn-danger">
+      <button class="btn btn-danger" onclick='borrarProducto("${producto.codigo}")'>
         <i class="bi bi-x-square"></i>
       </button>
     </td>
   </tr>`;
   console.log(tablaProductos);
-  limpiarFormulario()
 }
 
-function guardarProductosEnLocalStorage(){
-    localStorage.setItem('listaProductosKey', JSON.stringify(listaProductos));
+function guardarProductosEnLocalStorage() {
+  localStorage.setItem("listaProductosKey", JSON.stringify(listaProductos));
 }
 cargaInicial();
+
+window.borrarProducto = function (codigo) {
+  //Alert para confirmar la eliminacion del producto
+  Swal.fire({
+    title: "Borrar producto",
+    text: "¿Esta seguro de eliminar el producto? Este proceso no se puede revertir",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#ffd803",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    console.log(result);
+    if (result.isConfirmed) {
+      //Buscar el producto en el arreglo y borrarlo
+      let copiaListaProductos = listaProductos.filter(
+        (itemProducto) => itemProducto.codigo != codigo
+      );
+      listaProductos = copiaListaProductos;
+      //Actualizar el localstorage
+      guardarProductosEnLocalStorage();
+      //Actualizar la tabla
+      borrarTabla();
+      cargaInicial();
+
+      // Alert de borrado exitoso
+      Swal.fire(
+        "Producto borrado",
+        "El producto se elimino correctamente",
+        "success"
+      );
+    }
+  });
+};
+function borrarTabla() {
+  let tablaProductos = document.querySelector("#tablaProductos");
+  tablaProductos.innerHTML = "";
+}
+
+window.modificarProducto = function (codigoBuscado) {
+  //Cambio de estado de la variable productoNuevo
+  productoNuevo = false;
+  //buscar en el arreglo el producto que quiero editar
+  let productoBuscado = listaProductos.find(
+    (producto) => producto.codigo === codigoBuscado
+  );
+
+  //Abrir ventana modal con el formulario cargado con todos los datos del producto que se selecciono
+  modalAdminProducto.show();
+  codigo.value = productoBuscado.codigo;
+  nombre.value = productoBuscado.nombre;
+  descripcion.value = productoBuscado.descripcion;
+  imagen.value = productoBuscado.imagen;
+  cantidad.value = productoBuscado.cantidad;
+  precio.value = productoBuscado.precio;
+  genero.value = productoBuscado.genero;
+};
+
+function actualizarProducto() {
+  //Tomar los datos cargados en el formulario, buscar el objeto que se esta editando y actualizar sus valores
+  let posicionProductoBuscado = listaProductos.findIndex(
+    (producto) => codigo.value === producto.codigo
+  );
+
+  //Modificar los valores dentro del arreglo
+  listaProductos[posicionProductoBuscado].nombre = nombre.value;
+  listaProductos[posicionProductoBuscado].descripcion = descripcion.value;
+  listaProductos[posicionProductoBuscado].imagen = imagen.value;
+  listaProductos[posicionProductoBuscado].cantidad = cantidad.value;
+  listaProductos[posicionProductoBuscado].precio = precio.value;
+  listaProductos[posicionProductoBuscado].genero = genero.value;
+  //Actualizar el localstorage
+  guardarProductosEnLocalStorage();
+  //Actualizar la tabla
+  borrarTabla();
+  cargaInicial();
+  //Alert de confirmacion de cambios
+  Swal.fire(
+    "El producto se actualizo correctamente",
+    "Los datos delproducto seleccionado fueron actualizados",
+    "success"
+  );
+  //cerrar la ventana modal
+  modalAdminProducto.hide();
+  //limpiar el formulario
+  limpiarFormulario();
+}
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,87 +1,80 @@
 //type mdule
-
-// / import { validarEmail } from "./validaciones";
-// const email = document.getElementById("email").value
-// const btnIdentificarse = document.getElementById("btn-identificarse");
-// btnIdentificarse.addEventListener("click", (() => {identificarse}))
-
-// const identificarse = () => {
-//     if(validarEmail(email)){
-//         Swal.fire('Any fool can use a computer')
-//     } else {
-//         Swal.fire({
-//             icon: 'error',
-//             title: 'Oops...',
-//             text: 'Something went wrong!',
-//             footer: '<a href="">Why do I have this issue?</a>'
-//           })
-//     }
-
 export function validarEmail(email){
-const regExp = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi
-    if(!regExp.test(email.value)){
-        email.className = "form-control invalid-feedback"
-        return false
-
-    } else {
-        email.className = "form-control valid-feedback"
-        return true
+    const regExp = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi
+        if(!regExp.test(email.value)){
+            email.className = "form-control invalid-feedback"
+            return false
+    
+        } else {
+            email.className = "form-control valid-feedback"
+            return true
+        }
     }
-}
-
-export function validarNombre(nombre){
-    if( nombre.value.trim().length >= 1 && nombre.value.trim().length <=50 ){
-        nombre.className = 'form-control is-valid';
+    
+    export function validarNombre(nombre){
+        if( nombre.value.trim().length >= 1 && nombre.value.trim().length <=50 ){
+            nombre.className = 'form-control is-valid';
+            return true;
+        }else{
+            nombre.className = 'form-control is-invalid';
+            return false;
+        }
+    }
+    
+    
+    export function validarImagen(imagen){
+    
+       let patron = /(?:(?:https?:\/\/))[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/=]*(\.jpg|\.png|\.jpeg))/g
+    
+        if(patron.test(imagen.value)){
+            imagen.className = 'form-control is-valid';
+            return true;
+        }else{
+            imagen.className = 'form-control is-invalid';
+            return false;
+        }
+    }
+    
+    export function validarDescription(descripcion){
+        let expReg = /^[a-zA-Z0-9][a-zA-Z0-9\s]+$/ig;
+        if(expReg.test(descripcion.value)){
+            descripcion.className = 'form-control is-valid';
+            return true;
+        }else{
+            descripcion.className = 'form-control is-invalid';
+            return false;
+        }
+    }
+    
+    export function validarGenero(genero){
+        if(genero.value !== ""){
+            genero.className = "form-control is-valid"
+            return true
+        } else {
+            genero.className = "form-control is-invalid"
+            return false
+        }
+    }
+    
+    export function validarCantidad(cantidad){
+        if(isNaN(cantidad)){
+        cantidad.className = "form-control is-valid"
+        console.log("DAtazo")
         return true;
-    }else{
-        nombre.className = 'form-control is-invalid';
-        return false;
+        } else {
+            cantidad.className="form-control is-invalid"
+            console.log("adsasdasDAtazo")
+            return false
+        }
     }
-}
-
-
-export function validarImagen(imagen){
-
-   let patron = /(?:(?:https?:\/\/))[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/=]*(\.jpg|\.png|\.jpeg))/g
-
-    if(patron.test(imagen.value)){
-        imagen.className = 'form-control is-valid';
+    export function validarPrecio(precio){
+        if(isNaN(precio)){
+        precio.className = "form-control is-valid"
+        console.log("DAtazo")
         return true;
-    }else{
-        imagen.className = 'form-control is-invalid';
-        return false;
+        } else {
+            precio.className="form-control is-invalid"
+            console.log("adsasdasDAtazo")
+            return false
+        }
     }
-}
-
-export function validarDescription(descripcion){
-    let expReg = /^[a-zA-Z0-9][a-zA-Z0-9\s]+$/ig;
-    if(expReg.test(descripcion.value)){
-        descripcion.className = 'form-control is-valid';
-        return true;
-    }else{
-        descripcion.className = 'form-control is-invalid';
-        return false;
-    }
-}
-
-export function validarGenero(genero){
-    if(genero.value !== ""){
-        genero.className = "form-control is-valid"
-        return true
-    } else {
-        genero.className = "form-control is-invalid"
-        return false
-    }
-}
-
-export function validarCantidad(cantidad){
-    if(isNaN(cantidad)){
-    cantidad.className = "form-control is-valid"
-    console.log("DAtazo")
-    return true;
-    } else {
-        cantidad.className="form-control is-invalid"
-        console.log("adsasdasDAtazo")
-        return false
-    }
-}
