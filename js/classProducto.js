31189410fb08346069c74b179a7eb7bb21ca9c62
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-09-14 10:07:16 -0300

    Correcion en la propiedad precio

--- js/classProducto.js
+++ js/classProducto.js
@@ -1,9 +1,10 @@
 export class Producto {
-  constructor(codigo, nombre, descripcion, imagen, stock, categoria) {
+  constructor(codigo, nombre, descripcion, imagen, precio, stock, categoria) {
     this.codigo = codigo;
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.imagen = imagen;
+    this.precio = precio;
     this.stock = stock;
     this.categoria = categoria;
     this.precioTotal = 1;
